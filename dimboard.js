a4c55975ba86b876d312a02d333c06440c232ef9
Dylan Lom <dylanlom@gmail.com>
2021-07-23 20:49:41 +1000

    Make pin.tags optional

--- dimboard.js
+++ dimboard.js
@@ -13,7 +13,7 @@ function getPins(url) {
 function renderPins(pins) {
     const list = document.querySelector("ul#pins")
     if (!list) throw new Error("Element ul#pins not found!")
-    const items = pins.map(pin => `<li><dim-pin href="${pin.href}" tags="${pin.tags.join(',')}" date="${pin.date}">${pin.title}</dim-pin></li>`)
+    const items = pins.map(pin => `<li><dim-pin href="${pin.href}" tags="${pin.tags?.join(',')}" date="${pin.date}">${pin.title}</dim-pin></li>`)
     list.innerHTML = items.join('\n')
 }
 
